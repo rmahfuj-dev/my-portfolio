8a8f1763433d4dee954ebf139fa9fe234f952895
rmahfuj-dev <rmahfujur782@gmail.com>
2026-01-15 10:04:34 +0600

    connected with mongodb and projects data get api created

---  backend/index.js
+++  backend/index.js
@@ -1,15 +1,41 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
-const port = 3000;
+const port = process.env.PORT
 app.use(express.json())
 app.use(cors())
 
 app.get('/', (req, res) => {
-    res.send("Server is active")
+  res.send("Server is active")
 })
 
+
+const client = new MongoClient(process.env.URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const portfolio = client.db('portfolio')
+    const projects = portfolio.collection('projects-data')
+
+    // api for getting all data
+    app.get("/projects", async (req, res) => {
+      const query = await projects.find({}).toArray()
+      res.status(200).json(query)
+    })
+  } finally {
+  }
+}
+run().catch(console.dir);
+
 app.listen(port, () => {
-    console.log(`Server running at port ${port}`)
+  console.log(`Server running at port ${port}`)
 })
